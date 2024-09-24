07e4bba4ab5a7097444430dc928307960db1d734
nagishettipraveena <158548294+nagishettipraveena@users.noreply.github.com>
2024-09-24 12:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('registration.html')
+    return render_template('ur.py')
 
 @app.route('/register', methods=['POST'])
 def register():
